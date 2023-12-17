09bebc6f8fda23d8c666024d11b870f408a536f0
Tom86 <tommy_m86@hotmail.com>
2023-12-17 17:13:26 +0100

    Add project files.

---
+++ GameCharacter.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bossfight
+{
+    internal class GameCharacter
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Strength { get; set; }
+        public int Stamina { get; set; }
+
+        public GameCharacter(int health, int strength, int stamina, string name)
+        {
+            Health = health;
+            Strength = strength;
+            Stamina = stamina;
+            OriginalStamina = Stamina;
+            Name = name;
+        }
+
+        public int OriginalStamina;
+
+        public void Fight(GameCharacter opponenet)
+        {
+            if (Stamina <= 0)
+            {
+                Console.WriteLine($"{Name} is out of stamina, the character must recharge.\n");
+                Recharge();
+            }
+            else
+            {
+                Stamina -= 10;
+
+                opponenet.Health -= Strength;
+                Console.WriteLine($"{Name} attacked the enemy.\n" +
+                                  $"{opponenet.Name} was dealt a damage of {Strength}.\n" +
+                                  $"{opponenet.Name} has {opponenet.Health}Hp left.\n" +
+                                  $"Stamina: {Stamina}\n");
+                Thread.Sleep(1500);
+            }
+        }
+
+
+        public void Recharge()
+        {
+            RechargeLog();
+            Stamina = OriginalStamina;
+        }
+
+        private static void RechargeLog()
+        {
+            Console.Write("Recharging");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(".");
+                Thread.Sleep(500);
+            }
+            Console.WriteLine("\n");
+
+            //int index = 0;
+            //while (index < 3)
+            //{
+            //    index++;
+            //    Console.Write(index); //1,2,3
+            //        ||
+            //    Console.Write(index); //0,1,2
+            //    index++;
+            //}
+
+            //int i = 0;
+            //while (i < 3)
+            //{
+            //    i++;
+            //    Console.Write(".");
+            //}
+
+
+            //Thread.Sleep(1000);
+            //Console.Write(".");
+            //
+            //Console.Write(".");
+            //Thread.Sleep(500);
+            //Console.Write(".");
+            //Thread.Sleep(500);
+            //Console.Write(".\n");
+            //Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Threading.Tasks.Dataflow;
+
+namespace Bossfight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunGame();
+        }
+
+        public static void RunGame()
+        {
+            GameCharacter Hero = new GameCharacter(100, 20, 40, "Hero");
+            GameCharacter Boss = new GameCharacter(400, 20, 10, "Boss");
+
+
+            Hero.Fight(Boss);
+            Thread.Sleep(1500);
+            Boss.Fight(Hero);
+
+            while (Hero.Health > 0 && Boss.Health > 0)
+            {
+                Hero.Fight(Boss);
+                if (Boss.Health == 0)
+                {
+                    Console.WriteLine($"{Hero.Name} won the fight!\n{Boss.Name} was defeated.");
+                    break;
+                }
+
+                Boss.Fight(Hero);
+                if (Hero.Health == 0)
+                {
+                    Console.WriteLine($"{Boss.Name} won the fight!\n{Hero.Name} was defeated.");
+                    break;
+                }
+            }
+        }
+    }
+}
